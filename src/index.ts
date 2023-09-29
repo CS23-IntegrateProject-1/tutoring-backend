983e0d529aad80f68aa0770c6959eb30f55f3f61
Chayapol Mahatthanachai <best37@hotmail.com>
2023-09-29 15:59:14 +0700

    [UPDATE] package

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  setTimeout(() => {
+    res.send("Hello World!");
+  }, 2000);
 });
 app.listen(port, () => {
   console.log(`Server up and running on port: ${port}`);
